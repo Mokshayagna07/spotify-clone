1a82283f91909242cea97f7940c75cae69a61558
moksha <gompasaikumar7@gmail.com>
2026-03-02 21:30:12 +0530

    added js functionality for backward and forward options

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let play = document.getElementById('play')
 let progressBar = document.getElementById('progressBar')
 let audio = new Audio('Audio/salaar.mp3')
+let forward = document.getElementById('forward')
+let backward = document.getElementById('backward')
+
+let songIndex = 0;
 
 play.addEventListener('click', () => {
   if (audio.paused || audio.currentTime == 0) {
@@ -16,10 +20,10 @@ play.addEventListener('click', () => {
 })
 
 audio.addEventListener('timeupdate', () => {
+  if (isNaN(audio.duration) || audio.duration === 0) return;
   let progress = (audio.currentTime / audio.duration) * 100
   progressBar.value = progress
   progressBar.style.background = `linear-gradient(to right,#177200ff ${progress}%,#333 ${progress}%)`
-
 })
 
 progressBar.addEventListener('input', function () {
@@ -28,27 +32,83 @@ progressBar.addEventListener('input', function () {
   audio.currentTime = (progressBar.value * audio.duration) / 100;
 })
 
-let playMusic = Array.from(document.getElementsByClassName('playMusic'))
-console.log(playMusic);
+let musicPlayerBtns = Array.from(document.getElementsByClassName('music-player-btn'));
 
 const makeAllPlay = () => {
-  playMusic.forEach((element) => {
-    element.classList.remove('fa-pause')
-    element.classList.add('fa-play')
-  })
-}
+  document.querySelectorAll('.playMusic').forEach((element) => {
+    element.classList.remove('fa-pause');
+    element.classList.add('fa-play');
+  });
+};
 
-playMusic.forEach((element) => {
-  element.addEventListener('click', (e) => {
+const songFiles = [
+  "salaar",
+  "chhichhore",
+  "devara",
+  "hii nanna",
+  "krack",
+  "pushpa",
+  "rrr"
+];
+
+musicPlayerBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
     makeAllPlay();
-    e.target.classList.remove('fa-play');
-    e.target.classList.add('fa-pause');
-    play.classList.remove('fa-circle-play')
-    play.classList.add('fa-circle-pause')
-    index=parseInt(e.target.id);
-    console.log(index)
-    audio.src=`Audio/${index}.mp3`;
-    audio.currentTime=0;
-    audio.play();
-  })
-})
+
+    // Get the inner play icon
+    let icon = btn.querySelector('.playMusic');
+    if (icon) {
+      icon.classList.remove('fa-play');
+      icon.classList.add('fa-pause');
+
+      // Change main play button icon to pause as well
+      play.classList.remove('fa-circle-play');
+      play.classList.add('fa-circle-pause');
+
+      // Get the song index (subtract 1 for 0-based array index)
+      songIndex = (parseInt(icon.id) - 1) % songFiles.length;
+
+      // Get correct filename using modulo (in case of >7 elements)
+      let songName = songFiles[songIndex];
+
+      // Only restart audio if we clicked a new song
+      if (!audio.src.includes(encodeURI(songName) + ".mp3")) {
+        audio.src = `Audio/${songName}.mp3`;
+        audio.currentTime = 0;
+      }
+      audio.play();
+    }
+  });
+});
+
+const playCurrentSong = () => {
+  let songName = songFiles[songIndex];
+  audio.src = `Audio/${songName}.mp3`;
+  audio.currentTime = 0;
+  audio.play();
+
+  // Update main play button
+  play.classList.remove('fa-circle-play');
+  play.classList.add('fa-circle-pause');
+
+  makeAllPlay();
+
+  // Highlight all instances of the current song globally logic
+  document.querySelectorAll('.playMusic').forEach((icon) => {
+    if ((parseInt(icon.id) - 1) % songFiles.length === songIndex) {
+      icon.classList.remove('fa-play');
+      icon.classList.add('fa-pause');
+    }
+  });
+};
+
+forward.addEventListener('click', () => {
+  songIndex = (songIndex + 1) % songFiles.length;
+  playCurrentSong();
+});
+
+backward.addEventListener('click', () => {
+  // Add length to ensure we don't get negative modulo
+  songIndex = (songIndex - 1 + songFiles.length) % songFiles.length;
+  playCurrentSong();
+});
